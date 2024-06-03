82e35023ee5f7f0b533ed90435351c973d27186a
David G <himynamesdave@users.noreply.github.com>
2024-06-03 13:40:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,189 @@
+## Overview
+
+A small wrapper to download data using cve2stix and cpe2stix, organising it into STIX bundles based on time ranges.
+
+## Before you get started
+
+If you do not want to backfill, maintain, or support your own CVE and CPE STIX objects check out CTI Butler which provides a fully manage database of these objects and more!
+
+https://www.ctibutler.com/
+
+## Install the script
+
+```shell
+# clone the latest code
+git clone https://github.com/muchdogesec/cxe2stix_helper -b main --recurse-submodules
+# create a venv
+cd cxe2stix_helper
+python3 -m venv cxe2stix_helper-venv
+source cxe2stix_helper-venv/bin/activate
+# install requirements
+pip3 install -r requirements.txt
+```
+
+## Setup configoration options
+
+You will need to create an `.env` file as follows;
+
+```shell
+cp .env.example .env
+```
+
+You will need to set your NVD API key. You can get on for free here: https://nvd.nist.gov/developers/request-an-api-key
+
+## Usage
+
+```shell
+python3 cxe2stix_helper.py \
+	--run_cve2stix boolean \
+	--run_cpe2stix boolean \
+	--last_modified_earliest date \
+	--last_modified_latest date \
+	--file_time_range dictionary
+```
+
+Where;
+
+* `run_cve2stix` (optional, boolean): will run the cve2stix script with settings defined
+	* default: `false`
+* `run_cpe2stix` (optional, boolean): will run the cpe2stix script with settings defined
+	* default: `false`
+* `last_modified_earliest` (required, date in format `YYYY-MM-DDThh:mm:ss`): used in the the cve2stix/cpe2stix config
+	* default: none
+* `last_modified_latest` (required, date in format `YYYY-MM-DDThh:mm:ss`): used in the the cve2stix/cpe2stix config
+	* default: none
+* `file_time_range` (optional): defines how much data should be packed in each output bundle. Use `d` for days, `m` for months, `y` for years. Note for months and years, bundles are packed per calendar month / year (see example for more info)
+	* default `1m` (1 month)
+
+Both scripts also use the following parameters that a user does not enter at the command line
+
+* `RESULTS_PER_PAGE=` parameter. The value of these is different between scripts. It is set in the `.env` file (but we don't recommend changing the defaults).
+* `NVD_API_KEY=` parameter. A user should set this value in a local `.env` file in the base of the cxe2stix_helper code. This API key can be obtained for free here: https://nvd.nist.gov/developers/start-here . Note, the scripts will work without it, but it is very likely you will run into rate limit errors, so this is HIGHLY recommended.
+
+### Example 1: Get 3 months of CPE data (split into STIX bundles of 1 month)
+
+```shell
+python3 cxe2stix_helper.py \
+	--run_cpe2stix \
+	--last_modified_earliest 2023-03-04T00:00:00 \
+	--last_modified_latest 2023-06-04T23:59:59 \
+	--file_time_range 1m
+```
+
+Will generate 4 bundle files:
+
+* `cpe-bundle-2023_03_04-2023_03_31.json`
+* `cpe-bundle-2023_04_01-2023_04_30.json`
+* `cpe-bundle-2023_05_01-2023_05_31.json`
+* `cpe-bundle-2023_06_01-2023_06_04.json`
+
+
+### Example 2: Get 3 days of CVE data (split into STIX bundles of 1 day)
+
+```shell
+python3 cxe2stix_helper.py \
+	--run_cve2stix \
+	--last_modified_earliest 2023-01-01T00:00:00 \
+	--last_modified_latest 2023-01-03T23:59:59 \
+	--file_time_range 1d
+```
+
+Will generate 3 bundle files:
+
+* `cve-bundle-2023_01_01-2023_01_01.json`
+* `cve-bundle-2023_01_02-2023_01_02.json`
+* `cve-bundle-2023_01_03-2023_01_03.json`
+
+### Example 3: Get 2 days of CVE and CPE data (split into STIX bundles of 2 months)
+
+```shell
+python3 cxe2stix_helper.py \
+	--run_cve2stix \
+	--run_cpe2stix \
+	--last_modified_earliest 2023-01-01T00:00:00 \
+	--last_modified_latest 2023-01-02T23:59:59 \
+	--file_time_range 2m
+```
+
+Will generate 2 bundle files:
+
+* `cpe-bundle-2023_01_01-2023_01_02.json`
+* `cve-bundle-2023_01_01-2023_01_02.json`
+
+## Why not run the scripts (cpe2stix / cve2stix) independently?
+
+The APIs can return a large amount of data, and downloading large time ranges in one run can cause an issue.
+
+We use a range of downstream tools that require STIX bundles in smaller sizes and with certain naming conventions.
+
+Which means you need to manually edit the .env files for many time ranges each time.
+
+cxe2stix_helper is designed to automate the process of downloading very large datasets whilst also allowing control on the output filenames.
+
+## Recommendations for running large backfills
+
+### CVE
+
+The first CVE published was `1988-10-01T04:00:00.000`. There are 250,888 at the time of writing, and this number increasing rapidly.
+
+Due to the volume and size of CVEs, we recommend iterating through the data in months. This means all bundles (especially those after 2018) will always be less than 100mb.
+
+Here is what we use;
+
+```shell
+python3 cxe2stix_helper.py \
+	--run_cve2stix \
+	--last_modified_earliest 2005-01-01T00:00:00 \
+	--last_modified_latest 2024-01-01T23:59:59 \
+	--file_time_range 1m
+``` 
+
+Note, whilst the first CVE was published in October 1988, it appears all CVEs published before 2005 were updated at the end of 2005 (or afterwards). The 
+
+Thus, the earliest CVEs have a last modified date in 2005.
+
+### CPE
+
+There are more CPEs (1,267,211 currently) than CVEs but the STIX objects created from them are smaller, and thus a smaller file size.
+
+Chunking the time range into 3 month periods will always deliver bundles less than 100mb.
+
+```shell
+python3 cxe2stix_helper.py \
+	--run_cpe2stix \
+	--last_modified_earliest 2007-01-01T00:00:00 \
+	--last_modified_latest 2024-01-01T23:59:59 \
+	--file_time_range 3m
+```
+
+The earliest CPEs have a last modified date in 2007.
+
+### Download the data
+
+Make life easy for yourself -- [we have also made the data produced by the above commands available to download freely in our cti_knowledge_base_store repository](https://github.com/muchdogesec/cti_knowledge_base_store).
+
+## Git submodule use
+
+We try and keep this repo in sync with the remote cve2stix / cpe2stix repos used as Git submodules when changes happen.
+
+Sometimes this is not always the case (either because we've forgot, or there are breaking changes).
+
+If it's the case we've forgotten, you can update the Git Submodules in this repo as follows:
+
+```shell
+cd cpe2stix
+git checkout main
+git pull
+cd ..
+cd cve2stix
+git checkout main
+git pull
+```
+
+## Support
+
+[Minimal support provided via the DOGESEC community](https://community.dogesec.com/).
+
+## License
+
+[Apache 2.0](/LICENSE).
---
+++ cxe2stix_helper.py
@@ -0,0 +1,193 @@
+import argparse
+import json
+import logging
+from pathlib import Path
+import re
+from datetime import datetime as dt, date, timedelta
+import shutil
+import subprocess
+import sys
+import calendar
+import time
+import requests
+
+import dotenv
+import runpy
+import os
+
+dotenv.load_dotenv()
+
+def valid_date(s):
+    try:
+        return dt.strptime(s, "%Y-%m-%dT%H:%M:%S")
+    except ValueError:
+        msg = f"Not a valid date: {s}. Please use the format `YYYY-MM-DDThh:mm:ss`."
+        raise argparse.ArgumentTypeError(msg)
+
+def parse_time_range(s):
+    UNITS = "d", "m", "y"
+    match = re.match(r'(\d+)(\w+)', s)
+    try:
+        num, unit = match.groups()
+        unit = unit.lower()
+        # print(num, unit, match.groups())
+    except BaseException as e:
+        raise argparse.ArgumentTypeError(f"could not parse `{s}`: should be in format `2d|1m|6m|1`")
+    if unit[0] not in UNITS:
+        raise argparse.ArgumentTypeError(f"{s} -- unrecognized time unit: {unit}")
+    if int(num) == 0:
+        raise argparse.ArgumentTypeError(f"prefix cannot be zero or negative: {s}")
+    return s
+
+def get_time_ranges(s, earliest: dt, latest: dt) -> list[tuple[dt, dt]]:
+    ONEDAY = timedelta(days=1)
+    ONESEC = timedelta(seconds=1)
+    match = re.match(r'(\d+)(\w+)', s)
+    num, unit = match.groups()
+    num = int(num)
+    output : list[tuple[dt, dt]] = []
+    unit = unit[0]
+    hi = earliest
+    while hi < latest:
+        lo = hi
+        for _ in range(num):
+            if unit == 'm':
+                _, days_in_month = calendar.monthrange(hi.year, hi.month)
+                hi = hi.replace(day=days_in_month, hour=23, minute=59, second=59)
+            if unit == 'd':
+                hi += ONEDAY
+            if unit == 'y':
+                hi = dt(hi.year, 12, 31, 23, 59, 59)
+            hi += ONESEC
+        hi -= ONEDAY
+        hi = hi.replace(hour=23, minute=59, second=59)
+        if hi >= latest:
+            hi = latest
+        output.append((lo, hi))
+        hi += ONESEC
+    return output
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Helper script for converting CVE and CPE data to STIX format.", allow_abbrev=True)
+    group = parser.add_argument_group()
+    group.add_argument("--run_cve2stix", help="Set to true to run CVE to STIX conversion", action="store_true")
+    group.add_argument("--run_cpe2stix", help="Set to true to run CPE to STIX conversion", action="store_true")
+    parser.add_argument("--last_modified_earliest", help="Earliest date for last modified filter", metavar="YYYY-MM-DDThh:mm:ss", required=True, type=valid_date)
+    latest = parser.add_argument("--last_modified_latest", help="Latest date for last modified filter", metavar="YYYY-MM-DDThh:mm:ss", required=True, type=valid_date)
+    parser.add_argument("--file_time_range", help="Time range for file processing (e.g., 1m)", default="1m", type=parse_time_range)
+    
+    args = parser.parse_args()
+
+        # Custom validation
+    if not (args.run_cve2stix or args.run_cpe2stix):
+        parser.error("At least one of --run_cve2stix and --run_cpe2stix must be set")
+
+    if args.last_modified_latest < args.last_modified_earliest:
+        raise argparse.ArgumentError(latest, "--last_modified_latest must not be earlier than --last_modified_earliest")
+
+    return args
+
+def main():
+    args = parse_args()
+    # Accessing the provided arguments
+    run_cve2stix = args.run_cve2stix
+    run_cpe2stix = args.run_cpe2stix
+    last_modified_earliest = args.last_modified_earliest
+    last_modified_latest = args.last_modified_latest
+    file_time_range = args.file_time_range
+
+    os.environ.update(dict(
+        CVE_LAST_MODIFIED_EARLIEST=last_modified_earliest.strftime("%Y-%m-%dT%H:%M:%S"),
+        CVE_LAST_MODIFIED_LATEST=last_modified_latest.strftime("%Y-%m-%dT%H:%M:%S"),
+    ))
+    sys.path.append("cpe2stix")
+    os.environ.update(dict(
+        CVE_LAST_MODIFIED_EARLIEST=last_modified_earliest.strftime("%Y-%m-%dT%H:%M:%S"),
+        CVE_LAST_MODIFIED_LATEST=last_modified_latest.strftime("%Y-%m-%dT%H:%M:%S"),
+    ))
+    sys.path.append("cve2stix")
+    import cpe2stix.main as cpe2stix
+    import cve2stix.main as cve2stix
+    cve_results_per_page = os.getenv("CVE2STIX_RESULTS_PER_PAGE")
+    cpe_results_per_page = os.getenv("CPE2STIX_RESULTS_PER_PAGE")
+    if run_cve2stix and not cve_results_per_page.isdigit():
+        raise Exception(f"env CVE2STIX_RESULTS_PER_PAGE should be int, got {cve_results_per_page}")
+    if run_cpe2stix and not cpe_results_per_page.isdigit():
+        raise Exception(f"env CPE2STIX_RESULTS_PER_PAGE should be int, got {cpe_results_per_page}")
+
+    print()
+    PARENT_PATH = Path("./output").absolute()
+    OBJECTS_PARENT = PARENT_PATH/"objects"
+    BUNDLE_PATH = PARENT_PATH/"bundles"
+
+    shutil.rmtree(PARENT_PATH, ignore_errors=True)
+
+    for start_date, end_date in get_time_ranges(file_time_range, last_modified_earliest, last_modified_latest):
+        # end_date = dt.combine(end_date, dt.max.time())
+        start_day, end_day = start_date.strftime('%Y_%m_%d-%H_%M_%S'), end_date.strftime('%Y_%m_%d-%H_%M_%S')
+
+
+        if run_cve2stix:
+            file_system = OBJECTS_PARENT/f"cve_objects-{start_day}-{end_day}"
+            file_system.mkdir(parents=True, exist_ok=True)
+            cprocess = start_celery("cve2stix.celery", "cve2stix", env=dict(RESULTS_PER_PAGE=cve_results_per_page))
+            bundle_name = f"cve-bundle-{start_day}-{end_day}.json"
+            celery_task = cve2stix.main(
+                filename=bundle_name,
+                config=cve2stix.Config(
+                    start_date=start_date.strftime("%Y-%m-%dT%H:%M:%S"),
+                    end_date=end_date.strftime("%Y-%m-%dT%H:%M:%S"),
+                    stix2_objects_folder=str(file_system),
+                    file_system=str(file_system),
+                    stix2_bundles_folder=str(BUNDLE_PATH),
+                    results_per_page=int(cve_results_per_page),
+                    nvd_api_key=os.getenv("NVD_API_KEY")
+                ),
+            )
+            celery_task.get() # wait for it
+            cprocess.kill()
+
+        if run_cpe2stix:
+            file_system = OBJECTS_PARENT/f"cpe_objects-{start_day}-{end_day}"
+            file_system.mkdir(parents=True, exist_ok=True)
+            cprocess = start_celery("cpe2stix.celery", "cpe2stix", env=dict(RESULTS_PER_PAGE=cpe_results_per_page))
+            bundle_name = f"cpe-bundle-{start_day}-{end_day}.json"
+            celery_task = cpe2stix.main(
+                filename=bundle_name,
+                config=cpe2stix.Config(
+                    start_date=start_date.strftime("%Y-%m-%dT%H:%M:%S"),
+                    end_date=end_date.strftime("%Y-%m-%dT%H:%M:%S"),
+                    stix2_objects_folder=str(file_system),
+                    file_system=str(file_system),
+                    stix2_bundles_folder=str(BUNDLE_PATH),
+                    results_per_page=int(cpe_results_per_page),
+                    nvd_api_key=os.getenv("NVD_API_KEY")
+                ),
+            )
+            celery_task.get() # wait for it
+            cprocess.kill()
+    if os.getenv('KEEP_OBJECTS_DIR') != "true":
+        shutil.rmtree(OBJECTS_PARENT, ignore_errors=True)
+
+CELERY_PROCESSES: subprocess.Popen = []
+
+def start_celery(path: str, cwd=".", env=None):
+    logging.info(f"starting celery: {path}")
+    args = ["celery", "-A", path, "--workdir", cwd, "worker", "--loglevel", "info", "--purge"]
+    p = subprocess.Popen(args, stdout=sys.stdout, stderr=sys.stderr)
+
+    logging.info(f"waiting 10 seconds for celery to initialize")
+    time.sleep(10)
+    CELERY_PROCESSES.append(p)
+    return p
+
+if __name__ == "__main__":
+    import os
+    print(json.dumps(sys.argv[1:]))
+    try:
+        main()
+    except Exception as e:
+        logging.exception(e)
+        logging.info("Killing all child processes")
+        for p in CELERY_PROCESSES:
+            p.kill()
